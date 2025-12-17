086657c53a78a6872da797ec5c863ce3f3a3602c
denism921Ru <denism921@gmail.com>
2025-12-17 16:02:31 +0300

    Update sheets_client.py

--- sheets_client.py
+++ sheets_client.py
@@ -4,4 +4,26 @@ from googleapiclient.discovery import build
 
 SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
 
-SCOPES = ["https://www]()
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+
+def get_service():
+    creds_json = os.getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON")
+    if not creds_json:
+        raise RuntimeError("GOOGLE_APPLICATION_CREDENTIALS_JSON is not set")
+
+    creds = Credentials.from_service_account_info(
+        eval(creds_json),
+        scopes=SCOPES
+    )
+
+    return build("sheets", "v4", credentials=creds)
+
+
+def append_row(sheet_name: str, values: list):
+    service = get_service()
+    service.spreadsheets().values().append(
+        spreadsheetId=SPREADSHEET_ID,
+        range=sheet_name,
+        valueInputOption="USER_ENTERED",
+        body={"values": [values]},
+    ).execute()
