62fc67d6ad3fada1e8c8b7eef3ad642dda0b2947
denism921Ru <denism921@gmail.com>
2025-12-17 12:59:38 +0300

    Update states.py

--- states.py
+++ states.py
@@ -1,9 +1,5 @@
 from aiogram.fsm.state import StatesGroup, State
 
 
-class CleaningStates(StatesGroup):
-    start_shift = State()
-    select_task = State()
-    start_cleaning = State()
-    checklist = State()
-    finish_cleaning = State()
+class RoleSelect(StatesGroup):
+    choosing = State()
